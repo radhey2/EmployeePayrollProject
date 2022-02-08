edd717c4db859132778564c5a124ee4d05894a30
Nikita <amarnikita09@gmail.com>
2022-02-08 10:01:21 +0530

    Added Employee Payroll Spring App Setup

---
+++ src/main/java/com/bridegelabz/employeepayrollapp/EmployeePayrollApp.java
@@ -0,0 +1,13 @@
+package com.bridegelabz.employeepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeePayrollApp.class, args);
+    }
+
+}
