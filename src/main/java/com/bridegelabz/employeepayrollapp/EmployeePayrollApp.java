8cabc606b5b05e6468f0adb2daab4941fbf00a29
Nikita <amarnikita09@gmail.com>
2022-02-11 02:43:35 +0530

    Refactored: Used Lambock Library for Logging

--- src/main/java/com/bridegelabz/employeepayrollapp/EmployeePayrollApp.java
+++ src/main/java/com/bridegelabz/employeepayrollapp/EmployeePayrollApp.java
@@ -3,11 +3,16 @@ package com.bridegelabz.employeepayrollapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollApp {
 
     public static void main(String[] args) {
+
         SpringApplication.run(EmployeePayrollApp.class, args);
+        log.info("Employee Payroll App Started");
     }
 
 }
