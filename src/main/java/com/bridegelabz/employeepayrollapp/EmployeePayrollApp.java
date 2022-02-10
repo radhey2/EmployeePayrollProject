165d77d8720b46ec2382e24a98f632da5a6c5963
Nikita <amarnikita09@gmail.com>
2022-02-11 03:48:19 +0530

    Added: Logging Levels, Logging Patterns based on app running on DEV, STAGING or PROD

--- src/main/java/com/bridegelabz/employeepayrollapp/EmployeePayrollApp.java
+++ src/main/java/com/bridegelabz/employeepayrollapp/EmployeePayrollApp.java
@@ -4,15 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class EmployeePayrollApp {
 
     public static void main(String[] args) {
-
-        SpringApplication.run(EmployeePayrollApp.class, args);
-        log.info("Employee Payroll App Started");
+        ApplicationContext context = SpringApplication.run(EmployeePayrollApp.class, args);
+        log.info("Employee Payroll App Started in {} Environment",
+                context.getEnvironment().getProperty("environment"));
     }
 
 }
