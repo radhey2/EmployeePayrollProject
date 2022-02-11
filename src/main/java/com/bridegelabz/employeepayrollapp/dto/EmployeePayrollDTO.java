dc906b6ec49e8c3bc8e238a75a7862480a5414a0
Nikita <amarnikita09@gmail.com>
2022-02-11 10:20:10 +0530

    Refactored: Ensuring validations for PayrollDTO

--- src/main/java/com/bridegelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridegelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,30 +1,36 @@
 package com.bridegelabz.employeepayrollapp.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.ToString;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.List;
 
 @ToString
 public class EmployeePayrollDTO {
-    @NotEmpty(message="Employee name cannot be empty")
+
     @Pattern(regexp = "[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
     public String name;
 
     @Min(value =  500, message = "Min wage should be more than 500")
     public long salary;
 
+    @Pattern(regexp = "male|female", message = "Gender needs to be male or female")
     public String gender;
 
+    @JsonFormat(pattern = "dd MMM yyyy")
+    @NotNull(message = "startDate should Not be Empty")
+    @PastOrPresent(message = "startDate should be past or today's date")
     public LocalDate startDate;
 
+    @NotBlank(message = "Note cannot Not be Empty")
     public String note;
 
+    @NotBlank(message = "profilePic cannot Not be Empty")
     public String profilePic;
 
+    @NotNull(message = "department cannot Not be Empty")
     public List<String> department;
 
 }
