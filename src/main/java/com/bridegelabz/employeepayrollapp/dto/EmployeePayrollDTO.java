a7ccaf2d0707d18a15c67b21af3900d495008895
Nikita <amarnikita09@gmail.com>
2022-02-11 10:06:32 +0530

    Refactored: added remaining properties to PayrollDTO and Model

--- src/main/java/com/bridegelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridegelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,9 +1,14 @@
 package com.bridegelabz.employeepayrollapp.dto;
 
+import lombok.ToString;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
+import java.util.List;
 
+@ToString
 public class EmployeePayrollDTO {
     @NotEmpty(message="Employee name cannot be empty")
     @Pattern(regexp = "[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
@@ -12,14 +17,14 @@ public class EmployeePayrollDTO {
     @Min(value =  500, message = "Min wage should be more than 500")
     public long salary;
 
-    public EmployeePayrollDTO(String name, long salary) {
-        this.name = name;
-        this.salary = salary;
-    }
+    public String gender;
 
-    @Override
-    public String toString() {
-        return "name='" + name + '\'' +
-                ", salary=" + salary;
-    }
+    public LocalDate startDate;
+
+    public String note;
+
+    public String profilePic;
+
+    public List<String> department;
+
 }
--- src/main/java/com/bridegelabz/employeepayrollapp/entity/EmployeePayrollData.java
+++ src/main/java/com/bridegelabz/employeepayrollapp/entity/EmployeePayrollData.java
@@ -3,19 +3,32 @@ package com.bridegelabz.employeepayrollapp.entity;
 import com.bridegelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Data
 public class EmployeePayrollData {
 
     private int employeeId;
     private String name;
     private long salary;
+    private String gender;
+    private LocalDate startDate;
+    private String note;
+    private String profilePic;
+    private List<String> department;
 
-    public EmployeePayrollData() {
-    }
+
+    public EmployeePayrollData() { }
 
     public EmployeePayrollData(int employeeId, EmployeePayrollDTO employeePayrollDTO) {
         this.employeeId = employeeId;
         this.name = employeePayrollDTO.name;
         this.salary = employeePayrollDTO.salary;
+        this.gender = employeePayrollDTO.gender;
+        this.note = employeePayrollDTO.note;
+        this.profilePic = employeePayrollDTO.profilePic;
+        this.startDate = employeePayrollDTO.startDate;
+        this.department = employeePayrollDTO.department;
     }
 }
